9bf94b9d65aa8c90e11a961006e652676d18ac8d
golden-orange <slaing@hotmail.com>
2018-04-24 11:19:32 -0400

    adds practice files for control flow, using for loops to return specific numbers within a range

---
+++ bootcamp/js/section 9 control flow/loops/script.js
@@ -0,0 +1,23 @@
+for (i = -10; i < 20; i++){
+  console.log(i)
+}
+
+for(u=1;u<40;u+=1){
+  if(u % 2 == 0){
+  console.log(u + " is an even number between 0 and 40.");
+  }
+}
+//
+for(o=300; o < 333; o++){
+  if(o % 2 == 1){
+    console.log(o + " is an odd number between 300 and 333.")
+    }
+}
+
+for (j = 5; j < 50; j++){
+  if(j % 5 === 0){
+    if(j % 3 === 0){
+  console.log(j + " is divisible by 5 and 3 and is between 5 and 50.")
+    }
+  }
+}
